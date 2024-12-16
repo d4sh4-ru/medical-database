786ea7eadb8516887d2bfe9b9fcf8b3ea0d72701
d4sh4 <denis675skul@gmail.com>
2024-12-16 12:11:37 +0300

    Актуализация тестовых данных по поисковым ссылкам.

--- data/test_data.sql
+++ data/test_data.sql
@@ -2,8 +2,13 @@
 
 SET search_path = core;
 
-INSERT INTO core.pharmacysearchtemplates (id, pharmacy_name, search_url_template)
+INSERT INTO core.pharmacy_search_templates (id, pharmacy_name, search_url_template)
 VALUES 
     (1, 'april', 'https://april.example.com/search?={text}'),
-    (2, 'eapteka', 'https://eapteka.example.com/search?={text}');
+    (2, 'eapteka', 'https://eapteka.example.com/search?={text}'),
+    (3, 'Планета здоровья', 'https://planetazdorovo.ru/search/?q={text}'),
+    (4, 'Аптечество', 'https://kirov.aptechestvo.ru/catalog/?q={text}'),
+    (5, 'Вита Экспресс', 'https://vitaexpress.ru/search/?q={text}'),
+    (6, 'Бережная Аптека', 'https://b-apteka.ru/search?q={text}'),
+    (7, 'Сердце Вятки', 'https://sr.farm/search/index.php?q={text}');
 
